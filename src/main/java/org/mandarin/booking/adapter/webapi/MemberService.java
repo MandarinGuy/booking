56aa8e0f10910eb1ce8c452aa5765fc2e2627239
YeaChan05 <qkenrdl05@gmail.com>
2025-08-04 18:06:20 +0900

    ensure unique UserId during member registration by adding existsByUserId check

--- src/main/java/org/mandarin/booking/adapter/webapi/MemberService.java
+++ src/main/java/org/mandarin/booking/adapter/webapi/MemberService.java
@@ -15,6 +15,8 @@ public class MemberService implements MemberRegisterer {
     @Override
     @Transactional
     public void register(MemberRegisterRequest request) {
+        if(memberJpaRepository.existsByUserId(request.userId()))
+            throw new IllegalArgumentException("이미 존재하는 회원입니다: " + request.userId());
         var newMember = Member.register(request);
         memberJpaRepository.save(newMember);
     }
--- src/main/java/org/mandarin/booking/persist/MemberJpaRepository.java
+++ src/main/java/org/mandarin/booking/persist/MemberJpaRepository.java
@@ -4,5 +4,5 @@ import org.mandarin.booking.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberJpaRepository extends JpaRepository<Member, Long> {
-
+    boolean existsByUserId(String userId);
 }
