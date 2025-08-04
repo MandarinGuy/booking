5496b58f830f5f96dce3d326601d795648a03896
YeaChan05 <qkenrdl05@gmail.com>
2025-08-04 16:46:24 +0900

    annotate Member domain as JPA entity with @Entity and IDENTITY strategy

---
+++ src/main/java/org/mandarin/booking/persist/MemberJpaRepository.java
@@ -0,0 +1,8 @@
+package org.mandarin.booking.persist;
+
+import org.mandarin.booking.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberJpaRepository extends JpaRepository<Member, Long> {
+
+}
