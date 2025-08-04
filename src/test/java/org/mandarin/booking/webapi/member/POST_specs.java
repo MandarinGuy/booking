9500a20555468683654a48978a7abb5915f6ff58
YeaChan05 <qkenrdl05@gmail.com>
2025-08-04 16:46:53 +0900

    verify database persistence in member registration API test

--- src/test/java/org/mandarin/booking/webapi/member/POST_specs.java
+++ src/test/java/org/mandarin/booking/webapi/member/POST_specs.java
@@ -6,6 +6,7 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 import org.junit.jupiter.api.Test;
 import org.mandarin.booking.BookingApplication;
 import org.mandarin.booking.domain.MemberRegisterRequest;
+import org.mandarin.booking.persist.MemberJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -22,12 +23,7 @@ public class POST_specs {
     ) {
 
         // Arrange
-        var request = new MemberRegisterRequest(
-                "testName",
-                "testId",
-                "testPassword",
-                "test@gmail.com"
-        );
+        var request = generateRequest();
 
         // Act
         var response = testRestTemplate.postForEntity(
@@ -38,5 +34,39 @@ public class POST_specs {
 
         // Assert
         assertThat(response.getStatusCode().value()).isEqualTo(200);
+    }
+
+    @Test
+    void 올바른_회원가입_요청을_하면_데이터베이스에_회원_정보가_저장된다(
+            @Autowired TestRestTemplate testRestTemplate,
+            @Autowired MemberJpaRepository memberRepository
+    ) {
+        // Arrange
+        var request = generateRequest();
+
+        // Act
+        testRestTemplate.postForEntity(
+                "/api/members",
+                request,
+                Void.class
+        );
+
+        // Assert
+        var matchingMember = memberRepository.findAll()
+                .stream()
+                .filter(member -> member.getUserId().equals(request.userId()))
+                .findFirst()
+                .orElseThrow();
+
+        assertThat(matchingMember).isNotNull();
+    }
+
+    private static MemberRegisterRequest generateRequest() {
+        return new MemberRegisterRequest(
+                "testName",
+                "testId",
+                "testPassword",
+                "test@gmail.com"
+        );
     }
 }
