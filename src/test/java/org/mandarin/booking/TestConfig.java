ad69dd703d9d5885cda384be125e582a8ce1ecd2
YeaChan05 <qkenrdl05@gmail.com>
2025-09-05 21:06:44 +0900

    feat: add ShowCommandRepository to IntegrationTestUtils for testing show schedule functionality

--- src/test/java/org/mandarin/booking/TestConfig.java
+++ src/test/java/org/mandarin/booking/TestConfig.java
@@ -3,6 +3,7 @@ package org.mandarin.booking;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.mandarin.booking.app.TokenUtils;
 import org.mandarin.booking.app.persist.MemberCommandRepository;
+import org.mandarin.booking.app.persist.ShowCommandRepository;
 import org.mandarin.booking.domain.member.SecurePasswordEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
@@ -12,10 +13,12 @@ import org.springframework.context.annotation.Bean;
 public class TestConfig {
     @Bean
     public IntegrationTestUtils integrationTestUtils(@Autowired MemberCommandRepository memberRepository,
+                                                     @Autowired ShowCommandRepository showRepository,
                                                      @Autowired TokenUtils tokenUtils,
                                                      @Autowired SecurePasswordEncoder securePasswordEncoder,
                                                      @Autowired ObjectMapper objectMapper,
                                                      @Autowired DocsUtils docsUtils) {
-        return new IntegrationTestUtils(memberRepository, tokenUtils, securePasswordEncoder, objectMapper, docsUtils);
+        return new IntegrationTestUtils(memberRepository, showRepository, tokenUtils, securePasswordEncoder,
+                objectMapper, docsUtils);
     }
 }
