306d57d114df61f1a99e54664bc9181294370117
YeaChan05 <qkenrdl05@gmail.com>
2025-09-07 17:31:58 +0900

    add HallException for handling venue-related errors

---
+++ src/main/java/org/mandarin/booking/domain/venue/HallException.java
@@ -0,0 +1,9 @@
+package org.mandarin.booking.domain.venue;
+
+import org.mandarin.booking.domain.DomainException;
+
+public class HallException extends DomainException {
+    public HallException(String message) {
+        super(message);
+    }
+}
