0c5ef6d767019ddad847fcca5eb334aca5e7efb6
YeaChan05 <qkenrdl05@gmail.com>
2025-09-07 17:32:07 +0900

    refactor IntegrationTest annotation for cleaner code

--- src/test/java/org/mandarin/booking/IntegrationTest.java
+++ src/test/java/org/mandarin/booking/IntegrationTest.java
@@ -4,6 +4,7 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
@@ -11,7 +12,7 @@ import org.springframework.context.annotation.Import;
         webEnvironment = RANDOM_PORT,
         classes = BookingApplication.class
 )
-@org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs
+@AutoConfigureRestDocs
 @Import(TestConfig.class)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface IntegrationTest {
