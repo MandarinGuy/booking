22698835b2eea2cabbb1311f1c2b2c27a9c729cf
YeaChan05 <qkenrdl05@gmail.com>
2025-09-08 15:06:10 +0900

    add table annotation to Show entity for database mapping

--- src/main/java/org/mandarin/booking/domain/show/Show.java
+++ src/main/java/org/mandarin/booking/domain/show/Show.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -18,6 +19,7 @@ import org.mandarin.booking.domain.AbstractEntity;
 import org.mandarin.booking.domain.show.ShowSchedule.ShowScheduleCreateCommand;
 
 @Entity
+@Table(name = "shows")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Show extends AbstractEntity {
