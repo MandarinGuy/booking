4afe8a4e80f68ae885e325ec690a09156ba01100
YeaChan05 <qkenrdl05@gmail.com>
2025-10-04 14:50:35 +0900

    refactor BCryptSecurePasswordEncoder to use PasswordEncoder interface and update tests accordingly

--- application/src/main/java/org/mandarin/booking/adapter/security/BCryptSecurePasswordEncoder.java
+++ application/src/main/java/org/mandarin/booking/adapter/security/BCryptSecurePasswordEncoder.java
@@ -2,21 +2,21 @@ package org.mandarin.booking.adapter.security;
 
 import lombok.RequiredArgsConstructor;
 import org.mandarin.booking.domain.member.SecurePasswordEncoder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 class BCryptSecurePasswordEncoder implements SecurePasswordEncoder {
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public String encode(String password) {
-        return bCryptPasswordEncoder.encode(password);
+        return passwordEncoder.encode(password);
     }
 
     @Override
     public boolean matches(String rawPassword, String encodedPassword) {
-        return bCryptPasswordEncoder.matches(rawPassword, encodedPassword);
+        return passwordEncoder.matches(rawPassword, encodedPassword);
     }
 }
